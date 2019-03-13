4dc7f87a6d00b6ed72d56870052ed869ac2af4e0
ZackMinott <zacharyminott1997@gmail.com>
2019-03-13 13:58:31 -0700

    Added Camera Shake, Flappy Jump, And Player Slam

---
+++ VGDA Spring '19/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Events;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField] private float shakeDuration = 0.3f;  //Time the camera shake effect will last
+    [SerializeField] private float shakeAmplitude = 1.2f;
+    [SerializeField] private float shakeFrequency = 2.0f;
+
+    private float shakeElapsedTime = 0f;
+
+    public CinemachineVirtualCamera virtualCamera;
+    private CinemachineBasicMultiChannelPerlin virtualCameraNoise;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (virtualCamera != null)
+            virtualCameraNoise =
+                virtualCamera.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+    }
+
+    void Update()
+    {
+        ShakeCamera();
+    }
+
+    void ShakeCamera()
+    {
+        if (virtualCamera != null && virtualCameraNoise != null)
+        {
+            //if Camera shake effect is still playing
+            if (shakeElapsedTime > 0)
+            {
+                //Set Cinemachine Camera Noise parameters
+                virtualCameraNoise.m_AmplitudeGain = shakeAmplitude;
+                virtualCameraNoise.m_FrequencyGain = shakeFrequency;
+
+                //Update Shake Timer
+                shakeElapsedTime -= Time.deltaTime;
+            }
+            else
+            {
+                //If camera shake effect is over, reset
+                virtualCameraNoise.m_AmplitudeGain = 0f;
+                shakeElapsedTime = 0f;
+            }
+        }
+    }
+
+    public void setDuration()
+    {
+        shakeElapsedTime = shakeDuration;
+    }
+}
---
+++ VGDA Spring '19/Assets/Scripts/Player.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Player Attributes")]
+    [SerializeField] private float upForce;
+    [SerializeField] private float smashForce;
+
+    private CameraShake cameraShake;
+    private Rigidbody2D rb2d;
+    private bool isDead = false;
+    private bool isSmashing = false;
+    private bool inAir = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        cameraShake = GetComponent<CameraShake>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+            FlappyJump();
+            Smash();
+
+            Debug.Log(inAir);
+
+        }
+    }
+
+    void FlappyJump()
+    {
+        if (Input.GetMouseButtonDown(0) && !isSmashing)
+        {
+            rb2d.velocity = Vector2.zero;
+
+            rb2d.AddForce(new Vector2(0, upForce));
+            //Checks to see if player is in the air
+            inAir = true;
+        }
+    }
+
+    //Add camera shake
+    void Smash()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && inAir)
+        {
+            rb2d.AddForce(new Vector2(0, -smashForce));
+
+            isSmashing = true;
+        }
+    }
+
+    //will need to grab side scrolling script and add post processing effect
+    void Dash()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            if (isSmashing)
+            {
+                //Add camera shake
+                cameraShake.setDuration();
+            }
+
+            //Checks to see if play is on the ground
+            inAir = false;
+            isSmashing = false;
+        }
+    }
+
+   
+}
