ccd61ac8d23414184bb73e0d54dddab96417b798
Graysire <graysire@dyrix.net>
2019-04-22 14:20:42 -0700

    Added Hydrant and projectile scripts

---
+++ VGDA Spring '19/Assets/Scripts/Enemies/HydrantEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HydrantEnemy : MonoBehaviour
+{
+    private float nextFirePoint; //the next time the cloud will jump
+    private bool fireReady; //whether the cloud is prepared to jump
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireReady = true;
+        nextFirePoint = Time.time + Random.Range(1f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireReady && Time.time >= nextFirePoint)
+        {
+            Object.Instantiate(projectile, new Vector3(transform.localPosition.x,transform.localPosition.y,0), new Quaternion(0,0,0,0));
+            fireReady = false;
+        }
+        else if (!fireReady)
+        {
+            nextFirePoint = Time.time + Random.Range(1f, 3f);
+            fireReady = true;
+        }
+    }
+}
---
+++ VGDA Spring '19/Assets/Scripts/Enemies/ProjectileScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    private ScrollingBackdrop[] backgroundSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundSpeed = FindObjectsOfType<ScrollingBackdrop>();
+
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(backgroundSpeed[0].speed * -1 * Time.deltaTime, 0));
+        if (transform.localPosition.x <= -50)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
